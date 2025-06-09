675715d647a58e2aee8a77b5034b1679db33d475
0xataru <0xataru@proton.me>
2025-06-09 19:04:57 +0300

    Update Header component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -9,19 +9,19 @@ export default function Header() {
           Mark Raiter
         </h1>
         <p className="text-sm text-gray-300 mb-6 font-light tracking-wide px-2 block sm:hidden">
-          Rationalist | Engineer | Anime Fan
+          Rationalist | SWE | Anime Fan
         </p>
         <p className="text-xl sm:text-3xl lg:text-4xl text-gray-300 mb-6 sm:mb-8 lg:mb-12 font-light tracking-wide px-4 hidden sm:block">
           Rationalist | Software Engineer | Anime Fan
         </p>
         <div className="flex flex-wrap sm:flex-row justify-center gap-1.5 sm:gap-4 lg:gap-6 mt-4 sm:mt-6 lg:mt-8 px-1">
-          <div className="px-1.5 sm:px-6 lg:px-8 py-1 lg:py-4 bg-purple-600/20 rounded-full border border-purple-500/30 backdrop-blur-sm">
+          <div className="px-1 sm:px-6 lg:px-8 py-0.5 lg:py-4 bg-purple-600/20 rounded-full border border-purple-500/30 backdrop-blur-sm">
             <span className="text-purple-300 font-medium text-xxs sm:text-base lg:text-lg">Backend Development</span>
           </div>
-          <div className="px-1.5 sm:px-6 lg:px-8 py-1 lg:py-4 bg-blue-600/20 rounded-full border border-blue-500/30 backdrop-blur-sm">
+          <div className="px-1 sm:px-6 lg:px-8 py-0.5 lg:py-4 bg-blue-600/20 rounded-full border border-blue-500/30 backdrop-blur-sm">
             <span className="text-blue-300 font-medium text-xxs sm:text-base lg:text-lg">Cloud Infrastructure</span>
           </div>
-          <div className="px-1.5 sm:px-6 lg:px-8 py-1 lg:py-4 bg-amber-600/20 rounded-full border border-amber-500/30 backdrop-blur-sm">
+          <div className="px-1 sm:px-6 lg:px-8 py-0.5 lg:py-4 bg-amber-600/20 rounded-full border border-amber-500/30 backdrop-blur-sm">
             <span className="text-amber-300 font-medium text-xxs sm:text-base lg:text-lg">B2B SaaS</span>
           </div>
         </div>
