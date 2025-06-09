a73dcf07aa2bbb8181140e58a3736203c9805b4a
0xataru <0xataru@proton.me>
2025-06-09 14:02:47 +0300

    Add EmailIcon export to index and include test comment

--- src/components/index.ts
+++ src/components/index.ts
@@ -20,3 +20,5 @@ export { default as GitHubIcon } from "./icons/GitHubIcon";
 export { default as LinkedInIcon } from "./icons/LinkedInIcon";
 export { default as TwitterIcon } from "./icons/TwitterIcon";
 export { default as EmailIcon } from "./icons/EmailIcon"; 
+
+// test comment
